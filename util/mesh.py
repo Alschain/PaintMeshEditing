5f2b69093844b27a38393341aef67f5cc9c53f14
Wang Shuheng <33632398+Alschain@users.noreply.github.com>
2023-11-23 12:08:34 +0800

    Update mesh.py - add mesh with blendshapes

--- util/mesh.py
+++ util/mesh.py
@@ -1,4 +1,18 @@
+import torch
 from util.mesh_util import *
+
+blendshapes_names = [
+            "mouthClose", "mouthDimple_L", "mouthDimple_R", "mouthFrown_L", "mouthFrown_R",
+            "mouthFunnel", "mouthLeft", "mouthLowerDown_L", "mouthLowerDown_R", "mouthPress_L", "mouthPress_R",
+            "mouthPucker", "mouthRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower", "mouthShrugUpper",
+            "mouthSmile_L", "mouthSmile_R", "mouthStretch_L", "mouthStretch_R", "mouthUpperUp_L", "mouthUpperUp_R",
+            "noseSneer_L", "noseSneer_R", "jawForward", "jawLeft", "jawOpen", "jawRight",
+            "browDown_L", "browDown_R", "browInnerUp_L", "browInnerUp_R", "browOuterUp_L", "browOuterUp_R",
+            "cheekPuff_L", "cheekPuff_R", "cheekRaiser_L", "cheekRaiser_R", "cheekSquint_L", "cheekSquint_R",
+            "eyeBlink_L", "eyeBlink_R", "eyeLookDown_L", "eyeLookDown_R", "eyeLookIn_L", "eyeLookIn_R",
+            "eyeLookOut_L", "eyeLookOut_R", "eyeLookUp_L", "eyeLookUp_R", "eyeSquint_L", "eyeSquint_R",
+            "eyeWide_L", "eyeWide_R", "generic_neutral_mesh", "PupilDilate_L", "PupilDilate_R"
+            ]
 
 class Mesh:
     def __init__(self, vs=None, vns=None, vts=None, vcs=None, f_v_idx=None, f_vt_idx=None, f_vn_idx=None, tex=None):
@@ -26,7 +40,8 @@ class Mesh:
 
         self.edges = find_edges(self.f_v_idx, remove_duplicates=True) if self.f_v_idx is not None else None
         self.connected_faces = find_connected_faces(self.f_v_idx) if self.f_v_idx is not None else None
-    
+
+
     def clone(self):
         out = Mesh()
         if self.vs is not None:
@@ -56,9 +71,83 @@ class Mesh:
         if self.connected_faces is not None:
             out.connected_faces = self.connected_faces.clone()
         return out
-    
-    def update_vs(self, vs):
-        self.vs = vs
 
     def update_normal(self):
         self.vns, self.f_ns = compute_normals(self.vs, self.f_v_idx)
+
+
+class MeshWithBlendShapes():
+    def __init__(self, base_mesh, blendshapes_names, bs_meshes:dict):
+        self.base_mesh = base_mesh.clone()
+        self.device = base_mesh.vs.device
+
+        self.blendshapes_names = blendshapes_names
+        
+        self.bs_meshes = {}
+        for key in self.blendshapes_names:
+            if key in bs_meshes.keys():
+                self.bs_meshes[key] = bs_meshes[key].clone()
+            else:
+                self.bs_meshes[key] = None
+
+        
+        self.bs_offsets           = {}
+        self.trainable_indices    = {}
+        self.trainable_parameters = {}
+
+        self.get_bs_parameters()
+    
+    def get_bs_parameters(self, eps=1e-6):
+        for bs_name, bs_mesh in self.bs_meshes.items():
+            if bs_mesh is not None:
+                transformed_vertices_placeholder = torch.sqrt(torch.sum((bs_mesh.vs - self.base_mesh.vs)**2, dim=-1)) > eps
+                transformed_vertices_index = transformed_vertices_placeholder.nonzero(as_tuple=True)[0].long()
+                if transformed_vertices_index.shape[0] != 0:
+                    self.bs_offsets[bs_name] = (bs_mesh.vs - self.base_mesh.vs)[transformed_vertices_index]
+                    self.trainable_indices[bs_name] = transformed_vertices_index
+                    self.trainable_parameters[bs_name] = torch.ones(transformed_vertices_index.shape[0]).to(self.device)
+                else:
+                    self.bs_offsets[bs_name] = None
+                    self.trainable_indices[bs_name] = None
+                    self.trainable_parameters[bs_name] = None
+        return
+    
+    def set_bs_trainable(self):
+        for key in self.trainable_parameters.keys():
+            if key is not None:
+                self.trainable_parameters[key].requires_grad_ = True
+        return
+    
+    def set_bs_untrainable(self):
+        for key in self.trainable_parameters.keys():
+            if key is not None:
+                self.trainable_parameters[key].requires_grad_ = False
+        return
+
+    def clone(self):
+        out = MeshWithBlendShapes(self.base_mesh, self.blendshapes_names, self.bs_meshes)
+
+        out.bs_offsets = {}
+        out.trainable_indices = {}
+        out.trainable_parameters = {}
+
+        for key in self.blendshapes_names:
+            out.bs_offsets[key] = self.bs_offsets[key].clone() if self.bs_offsets[key] is not None else None
+            out.trainable_indices[key] = self.trainable_indices[key].clone() if self.trainable_indices[key] is not None else None
+            out.trainable_parameters[key] = self.trainable_parameters[key].clone() if self.trainable_parameters[key] is not None else None
+
+        return out        
+
+    def apply_bs_weights(self, bs_weights:dict):
+        ret_mesh = self.base_mesh.clone()
+        for bs_name in self.blendshapes_names:
+            if self.bs_offsets[bs_name] is not None:
+                current_weight = 0.
+                if bs_name in bs_weights.keys():
+                    current_weight = bs_weights[bs_name]
+                
+                ret_mesh.vs[self.trainable_indices[bs_name]] = \
+                        ret_mesh.vs[self.trainable_indices[bs_name]] + \
+                        current_weight * self.bs_offsets[bs_name] * self.trainable_parameters[bs_name].detach().unsqueeze(-1)
+
+        return ret_mesh
